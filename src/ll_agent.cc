41b6a2c104044cd5200db732fe0b84031d11733b
Conrad Meyer <cemeyer@uw.edu>
2011-10-18 19:27:27 -0700

    implement getters/setters w/ locking; implement nicer exception-free zmq wrapper

--- src/ll_agent.cc
+++ src/ll_agent.cc
@@ -11,6 +11,8 @@
 #include <assert.h>
 #include <fcntl.h>
 
+#include <pthread.h>
+
 #include "../include/libconsentpp.h"
 #include "./ll_agent.h"
 #include "./util.h"
@@ -18,10 +20,17 @@
 namespace LibConsent {
 namespace LowLevel {
 
-Agent::Agent() : zmq_(new zmq::context_t(1)) {
+Agent::Agent() : zmq_(new zmqmm::context_t(1)) {
   log_callback_ = NULL;
   storage_put_ = NULL;
   storage_get_ = NULL;
+  unique_peer_number_ = -1;
+  message_timeout_interval_ = -1;
+  stats_messages_expected_ = 0;
+  stats_messages_received_ = 0;
+  num_peers_ = 1;
+
+  pthread_rwlock_init(&rwlock_, NULL);
 #ifdef LIBCONSENT_ASSERT_LOG_
   assert_log_fd_ =
     open(LIBCONSENT_ASSERT_LOG_, O_APPEND|O_CREAT|O_WRONLY, 0644);
@@ -32,24 +41,76 @@ Agent::Agent() : zmq_(new zmq::context_t(1)) {
 void Agent::set_log_callback(LogCallback callback) {
   LC_ASSERT(callback != NULL);
 
+  pthread_rwlock_wrlock(&rwlock_);
   log_callback_ = callback;
+  pthread_rwlock_unlock(&rwlock_);
 }
 
 void Agent::set_storage_callbacks(StoragePut putter, StorageGet getter) {
   LC_ASSERT(putter != NULL);
   LC_ASSERT(getter != NULL);
 
+  pthread_rwlock_wrlock(&rwlock_);
   storage_put_ = putter;
   storage_get_ = getter;
+  pthread_rwlock_unlock(&rwlock_);
+}
+
+int Agent::unique_peer_number() {
+  int res;
+
+  pthread_rwlock_rdlock(&rwlock_);
+  res = unique_peer_number_;
+  pthread_rwlock_unlock(&rwlock_);
+
+  return res;
+}
+
+void Agent::set_unique_peer_number(int n) {
+  LC_ASSERT(n >= 0);
+
+  pthread_rwlock_wrlock(&rwlock_);
+  unique_peer_number_ = n;
+  pthread_rwlock_unlock(&rwlock_);
+}
+
+int Agent::message_timeout_interval() {
+  int res;
+
+  pthread_rwlock_rdlock(&rwlock_);
+  res = message_timeout_interval_;
+  pthread_rwlock_unlock(&rwlock_);
+
+  return res;
+}
+
+void Agent::set_message_timeout_interval(int t) {
+  LC_ASSERT(t > 0);
+
+  pthread_rwlock_wrlock(&rwlock_);
+  message_timeout_interval_ = t;
+  pthread_rwlock_unlock(&rwlock_);
+}
+
+double Agent::get_timeout_percent() {
+  int64_t expected, received;
+
+  pthread_rwlock_rdlock(&rwlock_);
+  expected = stats_messages_expected_;
+  received = stats_messages_received_;
+  pthread_rwlock_unlock(&rwlock_);
+
+  if (!expected) return 0.0;
+  double timedout = expected - received;
+  return timedout / expected;
 }
 
 void Agent::AddPeers(const char *zmq_str, int num_peers) {
+  LC_ASSERT(num_peers > 0);
 }
 
 void Agent::RemovePeers(const char *zmq_str, int num_peers) {
-}
-
-void Agent::SetUniquePeerNumber(int n) {
+  LC_ASSERT(num_peers > 0);
 }
 
 void Agent::AddBind(const char *zmq_str) {
@@ -59,9 +120,20 @@ void Agent::RemoveBind(const char *zmq_str) {
 }
 
 void Agent::Start(bool recover) {
+  LC_ASSERT(log_callback_);
+  LC_ASSERT(storage_put_);
+  LC_ASSERT(storage_get_);
+  LC_ASSERT(unique_peer_number_ >= 0);
+  LC_ASSERT(message_timeout_interval_ >= 0);
+  LC_ASSERT(unique_peer_number_ < num_peers_);
+  LC_ASSERT(num_peers_ > 2);
 }
 
 void Agent::Submit(const char *value, int value_len) {
+  LC_ASSERT(value_len >= 0);
+
+  // TODO(conrad): this will create a sock, connect to inproc://agent-submit or
+  // similar, thereby signalling the Start() thread to submit a new value.
 }
 
 }  // namespace LowLevel
--- src/ll_agent.h
+++ src/ll_agent.h
@@ -10,9 +10,8 @@
 
 #include <tr1/memory>
 
-#include <zmq.hpp>
-
 #include "../include/libconsentpp.h"
+#include "./zmqmm.h"
 
 using std::tr1::shared_ptr;
 
@@ -24,20 +23,30 @@ class Agent : public AgentInterface {
   Agent();
   virtual void set_log_callback(LogCallback callback);
   virtual void set_storage_callbacks(StoragePut putter, StorageGet getter);
+  virtual int unique_peer_number();
+  virtual void set_unique_peer_number(int n);
+  virtual int message_timeout_interval();
+  virtual void set_message_timeout_interval(int t);
+  virtual double get_timeout_percent();
+
   virtual void AddPeers(const char *zmq_str, int num_peers);
   virtual void RemovePeers(const char *zmq_str, int num_peers);
-  virtual void SetUniquePeerNumber(int n);
   virtual void AddBind(const char *zmq_str);
   virtual void RemoveBind(const char *zmq_str);
   virtual void Start(bool recover);
   virtual void Submit(const char *value, int value_len);
 
  private:
+  // These variables are run-time mutable and require locking:
+  pthread_rwlock_t rwlock_;
   LogCallback log_callback_;
   StoragePut storage_put_;
   StorageGet storage_get_;
+  int unique_peer_number_, message_timeout_interval_, num_peers_;
+  int64_t stats_messages_expected_, stats_messages_received_;
 
-  shared_ptr<zmq::context_t> zmq_;
+  // These are constant post-construction:
+  shared_ptr<zmqmm::context_t> zmq_;
 
 #ifdef LIBCONSENT_ASSERT_LOG_
   int assert_log_fd_;
